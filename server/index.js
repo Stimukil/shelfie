05a72be9dc36aba5fd7cfb2e6e09a4400bc1f599
unknown <jeff_cox76@hotmail.com>
2020-02-14 12:04:38 -0700

    this is a dumpster fire

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express")
+const massive = require("massive")
+const cors = require("cors")
+const app = express();
 
-const app = express()
 PORT = 3456
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
